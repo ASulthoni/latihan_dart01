77436f1ea335927bf11b3554e360fc8258756afc
A Sulthoni <asulthoni.freelancer@gmail.com>
2025-08-10 15:49:45 +0700

    masukan nama umur tinggi

--- bin/user_input.dart
+++ bin/user_input.dart
@@ -6,9 +6,14 @@ void main() {
   String? name  = stdin.readLineSync();
 
 // Mengambil input angka dari user
-  print("Masukan umurmu:");
+  print("Masukan Umurmu:");
   int? birthday = int.parse(stdin.readLineSync()!);
+// Mengambil input angka desimal dari user
+  print("Masukan Tinggi Badanmu:");
+  double number = double.parse(stdin.readLineSync()!);
+  
   print("Nama kamu adalah: ${name}");
-  print("Umur kamu adalah: ${birthday} tahun");
+  print("Umur kamu adalah: ${birthday} Tahun");
+  print("Tinggi badanmu adalah : $number" + " cm");
 }
 
