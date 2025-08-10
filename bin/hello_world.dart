567cbff214eb2963885514fa725a2d1e8e072b04
A Sulthoni <asulthoni.freelancer@gmail.com>
2025-08-10 20:26:45 +0700

    hello world

--- bin/hello_world.dart
+++ bin/hello_world.dart
@@ -1,5 +1,4 @@
-import 'package:latihan_dart01/latihan_dart01.dart' as latihan_dart01;
 
-void main(List<String> arguments) {
-  print('Hello world: ${latihan_dart01.calculate()}!');
+void main(){
+  print("Hello world");
 }
